370f970c6ee5b2279b56af82bc26f92eba4db22a
Yunho-Jun <wjsruddlf93@naver.com>
2023-05-30 23:23:46 +0900

    thymeleaf 실습

---
+++ src/main/java/org/zerock/b01/controller/SampleController.java
@@ -0,0 +1,31 @@
+package org.zerock.b01.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Log4j2
+@Controller
+public class SampleController {
+
+
+    @GetMapping("/hello")
+    public void hello(Model model){
+
+        log.info("hello......Controller");
+
+        model.addAttribute("msg" , "Hello World");
+    }
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model){
+
+        List<String> list = Arrays.asList("AAA","BBB","CCC","DDD");
+
+        model.addAttribute("list", list);
+    }
+}
