df4ab932d54c3af6f4d1a54cb0fd24d2bcbf22bc
Yunho-Jun <wjsruddlf93@naver.com>
2023-06-04 12:34:29 +0900

    register 기능 추가

--- src/main/java/org/zerock/b01/controller/BoardController.java
+++ src/main/java/org/zerock/b01/controller/BoardController.java
@@ -4,12 +4,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.BoardDTO;
 import org.zerock.b01.dto.PageRequestDTO;
 import org.zerock.b01.dto.PageResponseDTO;
 import org.zerock.b01.service.BoardService;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/board")
@@ -29,4 +34,35 @@ public class BoardController {
         model.addAttribute("responseDTO", responseDTO);
 
     }
+
+    @GetMapping("/register")
+    public void registerGET(){
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes){
+
+        log.info("board POST register .......");
+
+        if(bindingResult.hasErrors()){
+            log.info("has errors.....");
+
+            redirectAttributes.addFlashAttribute("errors",bindingResult.getAllErrors());
+
+            /*@Valid 부분에서 문제가 발생했을때 모든 에러를 errors라는 이름으로 redirectAttributes에 추가 전송한다*/
+            return "redirect:/board/register";
+        }
+        log.info(boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+
+    }
+
+
 }
